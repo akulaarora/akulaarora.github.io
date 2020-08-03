e1033f046d6cefae2978481924ac7adb0f74feca
Akul Arora <akularora@gmail.com>
2020-08-03 04:51:16 -0700

    Added Piotroski's F-scores

---
+++ precache-manifest.579cc2c77078d1f4106cf3448acc966d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aca4cff04f8d9ab8322ee2702daa205",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8cea1941af913a805f5a",
+    "url": "./static/js/2.ed9551ac.chunk.js"
+  },
+  {
+    "revision": "7edc9d6a5411dd85ffa426b7d3cb6e20",
+    "url": "./static/js/2.ed9551ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e34fe2cd99a48b2d29",
+    "url": "./static/js/main.4cb3b6bd.chunk.js"
+  },
+  {
+    "revision": "17fa9f993021625083f1",
+    "url": "./static/js/runtime-main.aa95ce29.js"
+  }
+]);
